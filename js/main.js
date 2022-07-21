c818f638f59b042210fdfe2a8d4ffec59b85870f
AnastasiyaKuzmitskaya <nataliletova2@gmail.com>
2022-07-22 00:06:27 +0300

    task_2

--- js/main.js
+++ js/main.js
@@ -5,4 +5,14 @@ let x = 20;
 let y = 58;
 let z = 42;
 let w=x+y+z;
-alert (w);
+console.log(w);
+
+
+//Task 2
+let SecondMin=60;
+let MinutesHour=60;
+let HoursDay=24;
+let DaysYears=365;
+let myAge=25;
+let myAgeInSeconds=SecondMin*MinutesHour*HoursDay*DaysYears*myAge;
+console.log(myAgeInSeconds);
