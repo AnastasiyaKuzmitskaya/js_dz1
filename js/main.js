63407f40250e991d624c7602245dcf24ce493018
AnastasiyaKuzmitskaya <nataliletova2@gmail.com>
2022-07-22 00:18:13 +0300

    task_3

--- js/main.js
+++ js/main.js
@@ -16,3 +16,22 @@ let DaysYears=365;
 let myAge=25;
 let myAgeInSeconds=SecondMin*MinutesHour*HoursDay*DaysYears*myAge;
 console.log(myAgeInSeconds);
+
+
+//Task 3
+let count = 42;
+let userName = '42';
+
+//
+
+
+console.log(`${count}`);
+
+let countString=String(count) ;
+console.log(countString);
+
+//
+console.log(+userName);
+
+let userNameNumber=Number(userName); 
+console.log(userNameNumber); 
