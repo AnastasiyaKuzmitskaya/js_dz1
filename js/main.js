da983263a2559b2b03dd6b0a5e8ccb7846350620
AnastasiyaKuzmitskaya <nataliletova2@gmail.com>
2022-07-25 01:20:17 +0300

    task2_2

--- js/main.js
+++ js/main.js
@@ -76,6 +76,16 @@ num_1=num_1-num_2;
 
 console.log(num_1, num_2);
 
+//Task 2_2
 
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!";
+
+
+let cipher = codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1]+ codeWord5[1];
+console.log(cipher);
 
 
